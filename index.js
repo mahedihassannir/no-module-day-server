9446ce04b87629f77a38445de1c8e73a981a2364
mahedi6666655555 <mahedinir34678@gmail.com>
2023-05-13 10:29:19 +0600

    no module day server

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express")
+
+const app = express()
+
+const port = process.env.PORT || 5000
+
+// import cors
+const cors = require("cors")
+
+// middlewir
+app.use(cors())
+
+// convert in json
+app.use(express.json())
+
+// dot env 
+require("dotenv").config();
+
+
+// here is the config of mongodb
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_EMAIL}:${process.env.DB_PASS}@cluster0.obla9o6.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+// here is the config of mongodb ends
+
+
+
+
+
+
+
+
+
+
+// here is the font or root of the server response
+app.get('/', (req, res) => {
+
+    res.send("server is running ")
+
+})
+
+// here is the listining the server
+app.listen(port, () => {
+    console.log(`serve is running on port${port}`);
+})
+
